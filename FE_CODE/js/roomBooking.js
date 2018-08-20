bcb318860175cab8078d7092068d81dac9265343
Theja Dasari <theja.dasari@gmail.com>
2018-08-20 23:11:25 +0530

    Fixing reference mistakes

--- FE_CODE/js/roomBooking.js
+++ FE_CODE/js/roomBooking.js
@@ -66,12 +66,11 @@ function roomBookingController(httpServicesData, $q, $uibModal, $scope, $filter,
     }
 
     function doReservation(dates) {
-        var formattedDates = dates;
-        self.selectedDates = [];
-        if(formattedDates){
-            angular.forEach(formattedDates, function(selectedDate, key){
-                var formattedDates = $filter('date')(selectedDate, 'dd-MM-yyyy');
-                self.selectedDates.push(formattedDates); 
+        var formattedDates = [];
+        if (dates){
+            angular.forEach(dates, function(selectedDate, key){
+                var formattedDate = $filter('date')(selectedDate, 'dd-MM-yyyy');
+                formattedDates.push(formattedDate); 
             })
         }
 
@@ -99,7 +98,7 @@ function roomBookingController(httpServicesData, $q, $uibModal, $scope, $filter,
                 }
             }
            // console.log(self.reservationData)
-            httpServicesData.requestingRoomBookingService(self.selectedDates, self.reservationData).then(successRoomBookingObj, errorBookingSlots);
+            httpServicesData.requestingRoomBookingService(formattedDates, self.reservationData).then(successRoomBookingObj, errorBookingSlots);
 
         }, function () {
             //alert('Meeting details are required to do booking');
@@ -121,7 +120,7 @@ function roomBookingController(httpServicesData, $q, $uibModal, $scope, $filter,
         return self.selectedDate;
     }, function (newValue, oldValue) {
         // If user selected a new date or if the application is loading first time
-        if (newValue != oldValue || self.formattedDate === null){
+        if ( newValue && (newValue != oldValue || self.formattedDate === null)){
             var formattedDate = $filter('date')(newValue, 'dd-MM-yyyy'); 
             if(self.formattedDate !== formattedDate){
                 self.formattedDate = formattedDate;
@@ -132,17 +131,6 @@ function roomBookingController(httpServicesData, $q, $uibModal, $scope, $filter,
         } 
     });
 
-    function formattongMultipleDates(dates){
-        var formattedDates = dates;
-        self.selectedDates = [];
-        if(formattedDates){
-            angular.forEach(formattedDates, function(selectedDate, key){
-                var formattedDates = $filter('date')(selectedDate, 'dd-MM-yyyy');
-                self.selectedDates.push(formattedDates) 
-            })
-        }
-        return self.selectedDates;
-    }
     self.logout = function(){
         httpServicesData.deleteSession().then(successfulLogOut,function(){
             console.log("log out not working")
@@ -177,15 +165,14 @@ function roomBookingController(httpServicesData, $q, $uibModal, $scope, $filter,
     self.roomsObj = [];
     self.reservationData = {}; // Current bookings from front end
     self.bookedMeetings = {}; // Already booked rooms of selected date
-    //self.selectedDate = new Date(); // Date Object from Calander
+    self.selectedDates = []; //multiple date selector
+    // self.selectedDate = new Date(); // Date Object from Calander
     self.formattedDate = null;
     self.getRoomAvailability = getRoomAvailability;
     self.bookSlot = bookSlot;
     self.clearSelection = clearSelection;
     self.doReservation = doReservation;
     self.deleteRoomReservation = deleteRoomReservation;
-    self.formattongMultipleDates = formattongMultipleDates;
-    self.selectedDates = []; //multiple date selector
     
     function checkUserSession() {
         httpServicesData.getSession().then(init, function () { 
